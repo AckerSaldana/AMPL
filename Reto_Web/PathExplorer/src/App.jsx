14a9127bb2462bb8f7ce98afb2c0d5bc793fd442
AckerSaldana <alcker100@hotmail.com>
2025-03-17 20:24:27 -0600

    ADD: partial routes layout and Navbar first design

--- Reto_Web/PathExplorer/src/App.jsx
+++ Reto_Web/PathExplorer/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx o donde configures tus rutas
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Profiles from './pages/Profiles';
+import Analytics from './pages/Analytics';
+import Settings from './pages/Settings';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="profiles" element={<Profiles />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ Reto_Web/PathExplorer/src/pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Typography, Paper, Grid, Card, CardContent } from '@mui/material';
+
+import useBodyStyles from '../hooks/useBodyStyles';
+
+const Dashboard = () => {
+  
+
+  return (
+    
+      <Typography variant="h4" gutterBottom>
+        Aqui va el dashboard pepe jejeje ou yea arriba las chivas
+
+        </Typography>
+
+    
+  );
+};
+
+export default Dashboard;
