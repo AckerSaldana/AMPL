8e7d7821e2ae947a2b93b4122079b1fea90bddab
Acortes1403 <ajclrakirute@gmail.com>
2025-05-28 18:50:19 -0600

    Add: Cypress Test 4

--- Reto_Web/PathExplorer/cypress.config.js
+++ Reto_Web/PathExplorer/cypress.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    baseUrl: 'https://prod-ampl.web.app',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
